45e4113d1f383ab0fea23b4ff76186492d8e3426
Shefoo23 <youssefashraf23799@outlook.com>
2025-07-10 00:35:27 +0300

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,18 +1,18 @@
-user = [{"name":"jack","pass":"2345"},{"name":"rose","pass":"3344"}]
-userInput=input("enter ur name:  ")
-def check_user_fun():
+   
+def check_user_fun(userinput):
+ user = [{"name":"jack","pass":"2345"},{"name":"rose","pass":"3344"}]
+
  for e in user:
-    if e["name"] ==userInput:
+    if e["name"] ==userinput:
         userPass=input("enter ur pass:  ")
         if e ["pass"] ==userPass:
             print("ur pass correct")
             break
         else:
-            print("uncorrect password")
+            print("incorrect password")
         break
     
     else:
         continue
  else:
     print("not found")
-check_user_fun()
